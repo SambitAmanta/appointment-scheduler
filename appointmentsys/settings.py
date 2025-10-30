93c784f700d8690ded10b4aefe6f33974408a7c2
SambitAmanta <sambit3535@gmail.com>
2025-10-31 01:26:49 +0530

    Add Celery and Redis configuration for async tasks

--- appointmentsys/settings.py
+++ appointmentsys/settings.py
@@ -134,3 +134,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# CELERY CONFIG
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'TZ_Asia/Kolkata'  # or UTC
+CELERY_BEAT_SCHEDULE = {}
